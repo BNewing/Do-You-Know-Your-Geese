fffb9d1337fe0410b36fbd78d380c6c19c5a639f
BNewing <bnewing92@hotmail.co.uk>
2016-11-22 17:28:36 +0000

    Move across of Firebase Script into js file

--- contact.js
+++ contact.js
@@ -1,3 +1,12 @@
+var config = {
+  apiKey: "AIzaSyDxoXiZy8-3C9Ose9EoT1juFGNF8THoi0g",
+  authDomain: "do-you-know-your-geese.firebaseapp.com",
+  databaseURL: "https://do-you-know-your-geese.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "632660019254"
+};
+firebase.initializeApp(config);
+
 function writeUserData() {
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
